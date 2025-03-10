3b71ef4d6b484902362c625cb54c47cc516d648d
alexrods <alexrodsanch@gmail.com>
2025-03-10 15:10:26 -0600

    Feature: send to multiple numbers

--- app.py
+++ app.py
@@ -13,7 +13,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 WEBHOOK_TOKEN=os.environ["WEBHOOK_TOKEN"]
-SEND_TO=os.environ["PHONE_NUMBER"]
+
+DEV=os.environ["PHONE_NUMBER"]
+HECTOR_PHONE_NUMBER = os.environ["HECTOR_PHONE_NUMBER"]
+TANIA_PHONE_NUMBER = os.environ["TANIA_PHONE_NUMBER"]
+LUIS_PHONE_NUMBER = os.environ["LUIS_PHONE_NUMBER"]
+VICTORINO_PHONE_NUMBER = os.environ["VICTORINO_PHONE_NUMBER"]
+
 TOTAL_LTC_WORKERS = 30
 TOTAL_BTC_WORKERS = 2
 
@@ -29,6 +35,8 @@ async def check_active_miners():
     # Intervalo de notificación en alerta: 5 minutos (300 segundos)
     alert_interval = 5 * 60
 
+    TO_NUMBERS = [DEV, HECTOR_PHONE_NUMBER, TANIA_PHONE_NUMBER, LUIS_PHONE_NUMBER, VICTORINO_PHONE_NUMBER]
+
     while True:
         workers = await monitor_api()
         ltc_workers = workers["response"].get("ltc_workers", np.nan)
@@ -40,20 +48,20 @@ async def check_active_miners():
         
         # Caso 1: Entramos en estado de alerta
         if current_alert_state and not in_alert:
-            send_message(SEND_TO, ltc_workers, btc_workers, in_alert=True)
+            send_message(TO_NUMBERS, ltc_workers, btc_workers, in_alert=True)
             last_alert_time = now
             in_alert = True
             print(f"Alerta inicial enviada: {datetime.now()}")
         
         # Caso 2: Seguimos en estado de alerta, verificar si toca reenviar
         elif current_alert_state and in_alert and (now - last_alert_time >= alert_interval):
-            send_message(SEND_TO, ltc_workers, btc_workers, in_alert=True)
+            send_message(TO_NUMBERS, ltc_workers, btc_workers, in_alert=True)
             last_alert_time = now
             print(f"Alerta repetida enviada: {datetime.now()}")
         
         # Caso 3: Salimos del estado de alerta
         elif not current_alert_state and in_alert:
-            send_message(SEND_TO, ltc_workers, btc_workers, in_alert=False)
+            send_message(TO_NUMBERS, ltc_workers, btc_workers, in_alert=False)
             in_alert = False
             last_alert_time = None
             print(f"Recuperación enviada: {datetime.now()}")
@@ -74,11 +82,11 @@ async def verify_token(request: Request):
     else:
         raise HTTPException(status_code=403, detail="Verification token mismatch")
 
+
 # Endpoint principal del webhook
 @app.post("/webhook")
 async def receive_whatsapp_message():
     return Response(status_code=200)
-
 
 
 @app.on_event("startup")
--- meta/send_message.py
+++ meta/send_message.py
@@ -13,53 +13,58 @@ app = FastAPI()
 WHATSAPP_API_URL=os.environ["WHATSAPP_API_URL"]
 WHATSAPP_ACCESS_TOKEN=os.environ["WHATSAPP_ACCESS_TOKEN"]
 
-def send_message(to, ltc_workers, btc_workers, in_alert):
-    if in_alert:
-        template = "workers_alert"
-    else:
-        template = "everything_ok_workers_"
-    try:
-        payload = json.dumps(
-            {
-                "messaging_product": "whatsapp",
-                "to": to,  
-                "type": "template",
-                "template": {
-                    "name": template,  
-                    "language": {
-                        "code": "en"
-                    },
-                    "components": [
-                        {
-                            "type": "body",
-                            "parameters": [
-                                {
-                                    "type": "text",
-                                    "text": ltc_workers
-                                },
-                                {
-                                    "type": "text",
-                                    "text": btc_workers
-                                }
-                            ]
-                        }
-                    ]
+def send_message(to_numbers, ltc_workers, btc_workers, in_alert):
+
+    if isinstance(to_numbers, str):
+        to_numbers = [to_numbers]
+
+    template = "workers_alert" if in_alert else "everything_ok_workers_"
+    test_template = "test_workers"
+
+    for to in to_numbers:
+        try:
+            payload = json.dumps(
+                {
+                    "messaging_product": "whatsapp",
+                    "to": to,  
+                    "type": "template",
+                    "template": {
+                        "name": template,  
+                        "language": {
+                            "code": "en"
+                        },
+                        "components": [
+                            {
+                                "type": "body",
+                                "parameters": [
+                                    {
+                                        "type": "text",
+                                        "text": ltc_workers
+                                    },
+                                    {
+                                        "type": "text",
+                                        "text": btc_workers
+                                    }
+                                ]
+                            }
+                        ]
+                    }
                 }
+            )
+
+            headers = {
+                'Authorization': f'Bearer {WHATSAPP_ACCESS_TOKEN}',
+                'Content-Type': 'application/json'
             }
-        )
 
-        headers = {
-            'Authorization': f'Bearer {WHATSAPP_ACCESS_TOKEN}',
-            'Content-Type': 'application/json'
-        }
-
-        response = requests.request("POST", WHATSAPP_API_URL, headers=headers, data=payload)
-        print(f"Response: {response.status_code} - {response.text}")
-    
-    except Exception as error:
-        return {
-            'error': True,
-            'code': 305,
-            'message': f'An error occurred while processing the request. {error}'
-        }
+            response = requests.request("POST", WHATSAPP_API_URL, headers=headers, data=payload)
+            print(f"Mensaje enviado a {to},\nResponse: {response.status_code} - {response.text}")
+            time.sleep(0.5)
+        
+        except Exception as error:
+            return {
+                'error': True,
+                'code': 305,
+                'message': f'An error occurred while processing the request. {error}'
+            }
     
